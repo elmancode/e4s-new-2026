0f5de16425f8beb62de0915886c8cb3640285435
elmanaskarov <elmanaskarov@gmail.com>
2026-02-11 20:12:28 +0000

    Initial commit

--- src/page/Projects.jsx
+++ src/page/Projects.jsx
@@ -24,10 +24,12 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { BsArrowRight } from "react-icons/bs";
 
 // Import Swiper styles
-import "swiper/css/effect-cards";
+import "swiper/css";
+import "swiper/css/navigation";
 import "swiper/css/pagination";
 
-import { EffectCards, Pagination, Autoplay } from "swiper/modules";
+// CORRECTED IMPORT: Removed 'Loop'
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
 
 const Projects = () => {
   useEffect(() => {
@@ -61,43 +63,39 @@ const Projects = () => {
       <div className="work_row">
       <div className="project">
           <Swiper
-            effect={"cards"}
-            grabCursor={true}
+            slidesPerView={1}
+            spaceBetween={30}
+            loop={true}
+            navigation
             pagination={{ clickable: true }}
-            autoplay={{ delay: 2000, disableOnInteraction: false }}
-            modules={[EffectCards, Pagination, Autoplay]}
+            autoplay={{ delay: 2500, disableOnInteraction: false }}
+            // CORRECTED MODULES: Removed 'Loop'
+            modules={[Navigation, Pagination, Autoplay]}
             className="mySwiper"
           >
             <SwiperSlide>
-              <img src={Photo1} alt="" />
+              <img src={Photo1} alt="MICS Project Slide 1" />
             </SwiperSlide>
             <SwiperSlide>
-              {" "}
-              <img src={Photo2} alt="" />
+              <img src={Photo2} alt="MICS Project Slide 2" />
             </SwiperSlide>
             <SwiperSlide>
-              {" "}
-              <img src={Photo3} alt="" />
+              <img src={Photo3} alt="MICS Project Slide 3" />
             </SwiperSlide>
             <SwiperSlide>
-              {" "}
-              <img src={Photo4} alt="" />
+              <img src={Photo4} alt="MICS Project Slide 4" />
             </SwiperSlide>
             <SwiperSlide>
-              {" "}
-              <img src={Photo5} alt="" />
+              <img src={Photo5} alt="MICS Project Slide 5" />
             </SwiperSlide>
             <SwiperSlide>
-              {" "}
-              <img src={Photo6} alt="" />
+              <img src={Photo6} alt="MICS Project Slide 6" />
             </SwiperSlide>
             <SwiperSlide>
-              {" "}
-              <img src={Photo7} alt="" />
+              <img src={Photo7} alt="MICS Project Slide 7" />
             </SwiperSlide>
             <SwiperSlide>
-              {" "}
-              <img src={Photo8} alt="" />
+              <img src={Photo8} alt="MICS Project Slide 8" />
             </SwiperSlide>
           </Swiper>
           <div className="text">
@@ -115,31 +113,30 @@ const Projects = () => {
 
         <div className="project">
           <Swiper
-            effect={"cards"}
-            grabCursor={true}
+            slidesPerView={1}
+            spaceBetween={30}
+            loop={true}
+            navigation
             pagination={{ clickable: true }}
-            autoplay={{ delay: 2000, disableOnInteraction: false }}
-            modules={[EffectCards, Pagination, Autoplay]}
+            autoplay={{ delay: 3000, disableOnInteraction: false }}
+            // CORRECTED MODULES: Removed 'Loop'
+            modules={[Navigation, Pagination, Autoplay]}
             className="mySwiper"
           >
             <SwiperSlide>
-              <img src={Eore1} alt="" />
+              <img src={Eore1} alt="EORE Project Slide 1" />
             </SwiperSlide>
             <SwiperSlide>
-              {" "}
-              <img src={Eore2} alt="" />
+              <img src={Eore2} alt="EORE Project Slide 2" />
             </SwiperSlide>
             <SwiperSlide>
-              {" "}
-              <img src={Eore3} alt="" />
+              <img src={Eore3} alt="EORE Project Slide 3" />
             </SwiperSlide>
             <SwiperSlide>
-              {" "}
-              <img src={Eore4} alt="" />
+              <img src={Eore4} alt="EORE Project Slide 4" />
             </SwiperSlide>
             <SwiperSlide>
-              {" "}
-              <img src={Eore5} alt="" />
+              <img src={Eore5} alt="EORE Project Slide 5" />
             </SwiperSlide>
           </Swiper>
 
@@ -158,31 +155,30 @@ const Projects = () => {
 
         <div className="project">
           <Swiper
-            effect={"cards"}
-            grabCursor={true}
+            slidesPerView={1}
+            spaceBetween={30}
+            loop={true}
+            navigation
             pagination={{ clickable: true }}
-            autoplay={{ delay: 2000, disableOnInteraction: false }}
-            modules={[EffectCards, Pagination, Autoplay]}
+            autoplay={{ delay: 3500, disableOnInteraction: false }}
+            // CORRECTED MODULES: Removed 'Loop'
+            modules={[Navigation, Pagination, Autoplay]}
             className="mySwiper"
           >
             <SwiperSlide>
-              <img src={EU1} alt="" />
+              <img src={EU1} alt="EU Endline Survey Slide 1" />
             </SwiperSlide>
             <SwiperSlide>
-              {" "}
-              <img src={EU2} alt="" />
+              <img src={EU2} alt="EU Endline Survey Slide 2" />
             </SwiperSlide>
             <SwiperSlide>
-              {" "}
-              <img src={EU3} alt="" />
+              <img src={EU3} alt="EU Endline Survey Slide 3" />
             </SwiperSlide>
             <SwiperSlide>
-              {" "}
-              <img src={EU4} alt="" />
+              <img src={EU4} alt="EU Endline Survey Slide 4" />
             </SwiperSlide>
             <SwiperSlide>
-              {" "}
-              <img src={EU5} alt="" />
+              <img src={EU5} alt="EU Endline Survey Slide 5" />
             </SwiperSlide>
             
           </Swiper>
